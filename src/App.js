a6be98834a1f3ad815d757bf2ae899af47658d2b
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-02-24 09:50:10 +0100

    App.css added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React, { Fragment } from 'react'
-// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import './App.css'
+// import Navbar from './components/layout/Navbar'
+// import Home from './components/pages/Home'
+// import About from './components/pages/About'
+
+// import ContactState from './components/context/contact/ContactState'
 
 const App = () => {
-  return <div>hello</div>
+  return <div>Hello App</div>
 }
 
 export default App
