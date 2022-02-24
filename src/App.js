e3a3dd233026b8ac13d584cdd2d3131b2b6293db
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-02-24 09:36:03 +0100

    Appjs modified

--- src/App.js
+++ src/App.js
@@ -1,28 +1,8 @@
 import React, { Fragment } from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import './App.css'
-import Navbar from './components/layout/Navbar'
-import Home from './components/pages/Home'
-import About from './components/pages/About'
-
-import ContactState from './components/context/contact/ContactState'
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 const App = () => {
-  return (
-    <ContactState>
-      <Router>
-        <Fragment>
-          <Navbar />
-          <div className='container'>
-            <Switch>
-              <Route exact path='/' component={Home} />
-              <Route exact path='/about' component={About} />
-            </Switch>
-          </div>
-        </Fragment>
-      </Router>
-    </ContactState>
-  )
+  return <div>hello</div>
 }
 
 export default App
