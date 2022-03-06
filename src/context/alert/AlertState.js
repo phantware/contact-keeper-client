52c46d0018c67285b3892a88aeaad344bb0bdfa6
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-03-06 15:59:41 +0100

    alert folder and states created

---
+++ src/context/alert/AlertState.js
@@ -0,0 +1,23 @@
+import { useReducer } from 'react'
+import * as uuid from 'uuid'
+import AlertContext from './AlertContext'
+import AlertReducer from './AlertReducer'
+import { SET_ALERT, REMOVE_ALERT } from '../types'
+
+const AlertState = ({ children }) => {
+  const initialState = []
+  const [state, dispatch] = useReducer(AlertReducer, initialState)
+
+  //Set Alert
+  const setAlert = (msg, type, timeout = 5000) => {
+    const id = uuid.v4()
+    dispatch({ type: SET_ALERT, payload: { msg, type, id } })
+    setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), timeout)
+  }
+
+  return (
+    <AlertContext.Provider value={{ alerts: state, setAlert }}>
+      {children}
+    </AlertContext.Provider>
+  )
+}
