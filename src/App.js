28c090c54a1c4bad07cc483c26f68cfb54d07650
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-03-06 15:31:40 +0100

    Login component created and added to appjs

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import About from './components/pages/About'
 import ContactState from './context/contact/ContactState'
 import AuthState from './context/auth/AuthState'
 import Register from './components/auth/Register'
+import Login from './components/auth/Login'
 
 const App = () => {
   return (
@@ -21,6 +22,7 @@ const App = () => {
                 <Route exact path='/' component={Home} />
                 <Route exact path='/about' component={About} />
                 <Route exact path='/register' component={Register} />
+                <Route exact path='/login' component={Login} />
               </Switch>
             </div>
           </Fragment>
